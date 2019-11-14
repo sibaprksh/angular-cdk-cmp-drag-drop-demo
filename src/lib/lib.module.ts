38caf3be36c71587b12523e6c6ab76ad49305939
sibaprksh <siba4269@gmail.com>
2019-11-14 18:05:44 +0530

    1 file modified

--- src/lib/lib.module.ts
+++ src/lib/lib.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DragComponent } from './drag/drag.component';
 import { DropComponent } from './drop/drop.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DragDropModule
   ],
   declarations: [DragComponent, DropComponent],
   exports: [DragComponent, DropComponent],
